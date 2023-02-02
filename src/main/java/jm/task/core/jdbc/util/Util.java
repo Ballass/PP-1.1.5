2d5c2b3e5a24c262be57ddacb61bf6244e00ae7e
Ballass <lesha.reutov@mail.ru>
2023-02-02 17:05:54 +0700

    fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,30 +1,15 @@
 package jm.task.core.jdbc.util;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class Util {
-    private static Connection con = null;
-    private static Util instance = null;
-
-    private Util() {
-        try {
-            if (null == con || con.isClosed()) {
-                Properties props = getProps();
-                con = DriverManager
-                        .getConnection(props.getProperty("db.url"), props.getProperty("db.username"), props.getProperty("db.password"));
-            }
-        } catch (SQLException | IOException e) {
-            e.printStackTrace();
-        }
-    }
+    private static Util instance;
+    private static Connection con;
+    private static final String URL = "jdbc:mysql://localhost:3306/Connection";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1597864";
 
     public static Util getInstance() {
         if (null == instance) {
@@ -34,16 +19,21 @@ public class Util {
     }
 
     public Connection getConnection() {
+        try {
+            if (null == con || con.isClosed()) {
+                con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return con;
     }
 
-    private static Properties getProps() throws IOException {
-        Properties props = new Properties();
-        try (InputStream in = Files.newInputStream(Paths.get(Util.class.getResource("/database.properties").toURI()))) {
-            props.load(in);
-            return props;
-        } catch (IOException | URISyntaxException e) {
-            throw new IOException("Database config file not found", e);
+    public static void closeConnection() {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
